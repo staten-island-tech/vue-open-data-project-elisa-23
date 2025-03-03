40ea0e1c9ae1f19792cbd9ac690de73084850060
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-03 12:18:04 -0500

    dsfsga

---
+++ app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink to="/">home</RouterLink>
+  <RouterLink to="/charts">charts</RouterLink>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
