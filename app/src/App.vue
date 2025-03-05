be56aed84c539fb7fc00bc27db8dae2031e9bfd8
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-05 12:33:57 -0500

    erstydrt

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <div class="dropdown dropdown-hover dropdown-right">
-    <div tabindex="0" role="button" class="btn m-1 h-12 w-14">
-      <img src="/hamburger.png" alt="hamburger button" class="cover h-5 w-5">
-    </div>
-    <ul tabindex="0" class="dropdown-content menu bg-black rounded-box z-1 w-52 p-2 shadow-sm">
-      <li>
-        <RouterLink to="/">
-          HOME
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/charts">
-          CHARTS
-        </RouterLink>
-      </li>
-    </ul>
-  </div>
+  <RouterLink to="/">
+    HOME
+  </RouterLink>
+  <RouterLink to="/charts">
+    CHARTS
+  </RouterLink>
   <RouterView />
 </template>
 
