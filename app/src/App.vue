60d28acdaaede6b9a11b249669e580dccf98357a
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-05 17:49:12 -0500

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <RouterLink to="/">
-    HOME
+    <button class="btn shadow-lg border-1 shadow-[#FDFBD7] border-[#FDFBD7] bg-[#756D47] text-[#FDFBD7] m-2">
+      HOME
+    </button>
   </RouterLink>
   <RouterLink to="/charts">
-    CHARTS
+    <button class="btn shadow-lg border-1 shadow-[#FDFBD7] border-[#FDFBD7] bg-[#756D47] text-[#FDFBD7] m-2">
+      CHARTS
+    </button>
   </RouterLink>
   <RouterView />
 </template>
