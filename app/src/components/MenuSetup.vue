4ab2862782e67f02b420ca712d86ac368e971346
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-05 19:09:16 -0500

    asdfghjkl

---
+++ app/src/components/MenuSetup.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex justify-center items-center flex-col flex-wrap m-5 h-full w-full">
+        <RouterLink to="/game" class="w-[15%] h-[6%] mb-4">
+        <button class="btn shadow-lg border-1 shadow-[#FDFBD7] border-[#FDFBD7] bg-[#756D47] text-[#FDFBD7] w-full h-full">
+          PLAY
+        </button>
+      </RouterLink>
+      <RouterLink to="/charts" class="w-[15%] h-[6%] mb-4">
+        <button class="btn shadow-lg border-1 shadow-[#FDFBD7] border-[#FDFBD7] bg-[#756D47] text-[#FDFBD7] w-full h-full">
+          STATS
+        </button>
+      </RouterLink>
+    </div>
+    <RouterView />
+  </template>
+  
+  <script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+  </script>
+  
+  <style scoped>
+  
+  </style>
