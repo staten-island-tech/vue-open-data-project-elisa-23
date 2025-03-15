518fd2ab2da541c7933b28c5497182902165fbf1
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-03-15 12:25:31 -0400

    Update PieChart.vue

---
+++ app/src/components/PieChart.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from "vue";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "vue-chartjs";
+
+// Register necessary Chart.js modules
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+// Define props for reusable data and options
+defineProps({
+  chartData: Object,
+  chartOptions: Object,
+});
+</script>
+
+<template>
+  <Pie :data="chartData" :options="chartOptions" />
+</template>
